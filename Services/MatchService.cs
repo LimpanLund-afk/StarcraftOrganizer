368fdc8a673ed37b0a0ee36147e637f5399751ed
LinusLund <lundlinus@live.se>
2025-05-12 21:19:03 +0200

    enkel UI för att loopa över PLayerCards. Justerat parametrar för PLayerCard. byggt logik för att räkna ut vinster/förluster/procent.
    lite funktionalitet in i MatchService

--- Services/MatchService.cs
+++ Services/MatchService.cs
@@ -1,6 +1,39 @@
-﻿namespace StarcraftOrganizer.Services
+﻿using Microsoft.EntityFrameworkCore;
+using StarcraftOrganizer.Data.DataContext;
+using StarcraftOrganizer.Data.Entities;
+
+
+namespace StarcraftOrganizer.Services
 {
-    public class MatchService
+    public class MatchService : BaseService
     {
+        private readonly IDbContextFactory<DataContext> _contextFactory;
+
+        public MatchService(IDbContextFactory<DataContext> factory, IDbContextFactory<DataContext> dbContextFactory) : base(factory)
+        {
+            _contextFactory = dbContextFactory;
+        }
+
+        public async Task<List<Match>> GetAllMatchessAsync()
+        {
+            using var context = await _contextFactory.CreateDbContextAsync();
+            return await context.Matches.ToListAsync();
+        }
+
+        public async Task<List<Match>> GetMatchesByChallengeIdAsync(int challengeId)
+        {
+            using var context = await _contextFactory.CreateDbContextAsync();
+            return await context.Matches
+                .Where(m => m.ChallengeId == challengeId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Match>> GetMatchesByPlayerIdAsync(int playerId)
+        {
+            using var context = await _contextFactory.CreateDbContextAsync();
+            return await context.Matches
+                .Where(m => m.Player1Id == playerId || m.Player2Id == playerId)
+                .ToListAsync();
+        }
     }
 }
